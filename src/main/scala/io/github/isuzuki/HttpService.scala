d166c22929e8a7c8e4c0454e3f7953fb1cac880a
isuzuki <ikkuntech@gmail.com>
2018-07-20 17:58:53 +0900

    added routing service

--- src/main/scala/io/github/isuzuki/HttpService.scala
+++ src/main/scala/io/github/isuzuki/HttpService.scala
@@ -1,20 +1,35 @@
 package io.github.isuzuki
 
+import com.twitter.finagle.http.path._
+import com.twitter.finagle.http.service.RoutingService
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.{Await, Future}
 
+object Router {
+  val service = RoutingService.byPathObject[Request] {
+    case Root / "item" / itemId => item(itemId)
+    case _ => index
+  }
+
+  val index: Service[Request, Response] = (req: Request) => {
+    val res = Response(req.version, Status.Ok)
+    res.setContentString("Hello World.")
+    res.setContentType("text/plain")
+    Future.value(res)
+  }
+
+  def item(itemId: String): Service[Request, Response] = (req: Request) => {
+    val res = Response(req.version, Status.Ok)
+    res.setContentString(s"$itemId item.")
+    res.setContentType("text/plain")
+    Future.value(res)
+  }
+}
+
 object HttpService {
   def main(args: Array[String]): Unit = {
-    val service = new Service[Request, Response] {
-      def apply(req: Request): Future[Response] = {
-        val res = Response(req.version, Status.Ok)
-        res.setContentString("Hello World.")
-        Future.value(res)
-      }
-    }
-
-    val server = Http.serve(":8080", service)
+    val server = Http.serve(":8080", Router.service)
     Await.ready(server)
   }
 }
